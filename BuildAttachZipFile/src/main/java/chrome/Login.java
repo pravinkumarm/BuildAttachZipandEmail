2caf69e8e94f51817510775d22df5597f19d8d52
LENOVO <LENOVO@DESKTOP-TPUHNFH>
2021-04-04 17:01:22 +0530

    latest

---
+++ BuildAttachZipFile/src/main/java/chrome/Login.java
@@ -0,0 +1,44 @@
+	package chrome;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+
+	RemoteWebDriver driver;
+
+	@Test
+	public void loginToLeafTaps() {
+                 	
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps");
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.close();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
